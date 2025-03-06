5f98a0b7bee16aba503da57060713a7b6a33a9cd
tumansutradhar <officialbishalsutradhar@gmail.com>
2025-03-06 18:43:12 +0530

    updated index file

--- index.js
+++ index.js
@@ -35,7 +35,7 @@
 
 // fs.writeFileSync("hello.txt", "Hello World");
 
-// console.log(__filename);
+// console.log(__fileName);
 
 // const gs = require("fs").writeFileSync;
 
@@ -118,24 +118,24 @@
 // const input = process.argv;
 
 // const command = input[2];
-// const filename = input[3];
+// const fileName = input[3];
 // const content = input[4];
 
 // if (command === "add") {
-//     if (!filename || !content) {
-//         console.log("Usage: node script.js add <filename> <content>");
+//     if (!fileName || !content) {
+//         console.log("Usage: node script.js add <fileName> <content>");
 //         process.exit(1);
 //     }
-//     fs.writeFileSync(filename, content);
-//     console.log(`File '${filename}' created with content: "${content}"`);
+//     fs.writeFileSync(fileName, content);
+//     console.log(`File '${fileName}' created with content: "${content}"`);
 // } else if (command === "remove") {
-//     if (!filename) {
-//         console.log("Usage: node script.js remove <filename>");
+//     if (!fileName) {
+//         console.log("Usage: node script.js remove <fileName>");
 //         process.exit(1);
 //     }
 //     try {
-//         fs.unlinkSync(filename);
-//         console.log(`File '${filename}' has been removed.`);
+//         fs.unlinkSync(fileName);
+//         console.log(`File '${fileName}' has been removed.`);
 //     } catch (error) {
 //         console.error(`Error: ${error.message}`);
 //     }
@@ -147,22 +147,22 @@
 
 // const fs = require("fs");
 // const path = require("path");
-// const dirname = path.join(__dirname, "files");
+// const dirName = path.join(__dirname, "files");
 
-// console.log("Target directory:", dirname);
+// console.log("Target directory:", dirName);
 
-// if (!fs.existsSync(dirname)) {
-//     fs.mkdirSync(dirname);
-//     console.log("Directory created:", dirname);
+// if (!fs.existsSync(dirName)) {
+//     fs.mkdirSync(dirName);
+//     console.log("Directory created:", dirName);
 // }
 
 // for (let i = 1; i <= 5; i++) {
-//     const filePath = path.join(dirname, `file${i}.txt`);
+//     const filePath = path.join(dirName, `file${i}.txt`);
 //     fs.writeFileSync(filePath, `This is file ${i}`);
 //     console.log(`Created: ${filePath}`);
 // }
 
-// fs.readdir(dirname, (error, files) => {
+// fs.readdir(dirName, (error, files) => {
 //     if (error) {
 //         console.error("Error reading directory:", error.message);
 //         return;
@@ -171,6 +171,69 @@
 //     } else {
 //         console.log("Files in directory:", files);
 //     }
+// });
+
+// --------------------------------------------------
+
+// const fs = require("fs");
+// const path = require("path");
+// const dirName = path.join(__dirname, "files");
+// const filePath = path.join(dirName, "file6.txt");
+// const oldPath = path.join(dirName, "file6.txt");
+// const newPath = path.join(dirName, "newFile6.txt");
+
+// fs.writeFileSync(filePath, "This is file 6");
+
+// fs.appendFile(filePath, "This is a new version of file 6.", "utf8", (err) => {
+//     if (err) {
+//         console.error("Error appending to file:", err);
+//         return;
+//     }
+// });
+
+// fs.readFile(filePath, "utf8", (err, item) => {
+//     if (err) {
+//         console.error("Error reading file:", err);
+//         return;
+//     }
+//     console.log(item);
+// });
+
+// fs.rename(oldPath, newPath, (err) => {
+//     if (err) {
+//         console.error("Error renaming file:", err);
+//         return;
+//     }
+//     console.log("File renamed successfully!");
+// });
+
+// fs.unlinkSync(newPath);
+
+// --------------------------------------------------
+
+// let a = 10;
+// let b = 10;
+
+// setTimeout(() => {
+//     b = 20;
+// }, 2000);
+
+// console.log(a + b);
+
+// --------------------------------------------------
+
+// let i = 10;
+// let n = 10;
+
+// let waitingData = new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//         resolve(20);
+//     }, 2000);
+// });
+
+// waitingData.then((d) => {
+//     n = d;
+//     console.log(i + n);
 // });
 
 // --------------------------------------------------
